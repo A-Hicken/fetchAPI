f2f0b40ec05cff09ff7124c7c738b8b148ce4704
A-Hicken <amberlie.hicken@gmail.com>
2023-11-15 14:37:27 -0700

    final update

--- script.js
+++ script.js
@@ -10,12 +10,7 @@ fetch(URL)
     let business = jsObject.business;
 
     for (let i = 0; i < business.length; i++) {
-      // console.log(business[i].name);
-      // You must include your javascript below to display the following information:
-      // 1. business names, 2.business image,
-      // 3. business locations, 4. business descriptions
-      // Here is an example for the name to start you off.
-
+      console.log(business[i]);
       // Creates card and places business name in h2 element
       let card = document.createElement("section");
       // Adds a classname to the section element above
@@ -24,11 +19,18 @@ fetch(URL)
       h2.textContent = business[i].name;
       card.appendChild(h2);
       document.querySelector("div.cards").appendChild(card);
-
-      // Create the image location, you can look up how to add image using javascript
-
-      // Include a business location
+      //addresses
+      let location = document.createElement("h2");
+      location.textContent = business[i].address;
+      card.appendChild(location);
 
       // Include a business Description
+      let paragraph = document.createElement("h2");
+      paragraph.textContent = business[i].description;
+      card.appendChild(paragraph);
+      // Create the image location, you can look up how to add image using javascript
+      let image = document.createElement("img");
+      image.setAttribute("src", business[i].imageurl);
+      card.appendChild(image);
     }
   });
